1221956f2f94db6e30afffb9dfee9227253891cc
saim-a11y <saim@thefivestarschool.org>
2025-11-20 08:43:44 +0000

    Add banking system implementation for deposits and withdrawals

--- first.py
+++ first.py
@@ -6,11 +6,39 @@ print(int(b))
 
 c = "5.6"
 print(float(c))
-print(int(float(c)))
-print(type(float(c)))
+print()
 
 d = 5
 print(complex(d))
 
 f = 5.6
 print(complex(f))
+2
+
+print("---------------------------------------------------")
+# operators
+x = 10
+y = 3
+print(x + y)  # addition
+print(x - y)  # subtraction
+print(x * y)  # multiplication
+print(x / y)  # division
+print(x // y) # floor division (It divides x by y and then rounds the result down to the nearest whole number.)
+print(x % y)  # modulus
+print(x ** y) # exponentiation
+print(-x)      # negation
+print(+y)      # unary plus (+y just returns the value of y without changing it.)
+print(abs(-x)) # absolute value (abs(...) returns the non-negative value of a number.)
+print(round(5.67)) # rounding
+print(round(5.67, 1)) # rounding to 1 decimal place
+print(pow(x, 2)) # power function
+print(divmod(x, y)) # quotient and remainder
+print(complex(x, y)) # complex number
+print(bin(x)) # binary representation
+
+
+print("---------------------------------------------------")
+
+a ,b = 5, 10
+min = a if a < b else b
+print(min)
---
+++ practice/Bank_sys.py
@@ -0,0 +1,25 @@
+# input name, account balance  and deposit or withdraw amount 
+# perform the operation and display the updated balance
+
+
+user_name = input("Enter your name: ")
+account_balance = float(input("Enter your account balance: "))
+operation = input("Do you want to deposit or withdraw? (d/w): ").lower()
+
+if user_name == "" or account_balance < 0:
+    print("Invalid input. Please enter a valid name and a non-negative account balance.")
+    exit()
+
+if operation == 'd':
+    deposit_amount = float(input("Enter the amount to deposit: "))
+    account_balance += deposit_amount
+    print(f"Deposit successful! Updated balance for {user_name} is: ${account_balance:.2f}")
+elif operation == 'w':
+    withdraw_amount = float(input("Enter the amount to withdraw: "))
+    if withdraw_amount > account_balance:
+        print("withdraw successful! Updated balance for {user_name} is: {withdraw_amount:.2f}")
+    else:
+        account_balance -= withdraw_amount
+        print(f"Withdrawal successful! Updated balance for {user_name} is: ${account_balance:.2f}")
+else:
+    print("Invalid operation. Please enter 'd' for deposit or 'w' for withdraw.")
