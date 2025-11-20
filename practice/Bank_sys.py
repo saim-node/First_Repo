177cc3bd15f9521a945c44550051023131489c37
saim-a11y <saim@thefivestarschool.org>
2025-11-20 09:23:38 +0000

    Refactor account balance handling and improve deposit/withdrawal messages

--- practice/Bank_sys.py
+++ practice/Bank_sys.py
@@ -1,26 +1,50 @@
-# input name, account balance  and deposit or withdraw amount 
+# input name, account balance and deposit or withdraw amount 
 # perform the operation and display the updated balance
 
-
 user_name = input("Enter your name: ")
-account_balance = float(input("Enter your account balance: "))
+try:
+    account_balance = float(input("Enter your account balance: "))
+except ValueError:
+    print("Invalid balance input.")
+    exit()
+
 operation = input("Do you want to deposit or withdraw? (d/w): ").lower()
 
 if user_name == "" or account_balance < 0:
     print("Invalid input. Please enter a valid name or account balance.")
     exit()
 
+# function
+def print_account_info(balance, name, withdraw_amount=0, deposit_amount=0):
+    print(f"\nAccount holder: {name}")
+    if withdraw_amount > 0:
+        print(f"Attempted withdrawal amount: {withdraw_amount}")
+    elif deposit_amount > 0:
+        print(f"Deposit amount: {deposit_amount}")
+    print(f"Current balance: {balance}\n")
+
 if operation == 'd':
     deposit_amount = float(input("Enter the amount to deposit: "))
+    if deposit_amount <= 0:
+        print("Deposit amount must be greater than 0.")
+        exit()
     account_balance += deposit_amount
-    print(f"Deposit successful! Updated balance for {user_name} is: ${account_balance:.2f}")
+    print_account_info(account_balance, user_name, deposit_amount=deposit_amount)
+    print("Deposit successful!")
 
 elif operation == 'w':
     withdraw_amount = float(input("Enter the amount to withdraw: "))
+    if withdraw_amount <= 0:
+        print("Withdrawal amount must be greater than 0.")
+        exit()
     if withdraw_amount > account_balance:
-        print("withdraw successful! Updated balance for {user_name} is: {withdraw_amount:.2f}")
+        print_account_info(account_balance, user_name, withdraw_amount=withdraw_amount)
+        print("SORRY! Cannot withdraw more than the current balance.")
+        exit()
     else:
         account_balance -= withdraw_amount
-        print(f"Withdrawal successful! Updated balance for {user_name} is: {account_balance:.2f}")
+        print_account_info(account_balance, user_name, withdraw_amount=withdraw_amount)
+        print("Withdrawal successful!")
+
 else:
     print("Invalid operation. Please enter 'd' for deposit or 'w' for withdraw.")
