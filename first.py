2c5412b2f6966b1ceb583a3fe693209ce9745b4c
saim-a11y <saim@thefivestarschool.org>
2025-11-20 07:58:43 +0000

    Add initial implementation of type conversion examples in first.py

---
+++ first.py
@@ -0,0 +1,16 @@
+a = 2
+print(float(a))
+
+b = 5.6
+print(int(b))
+
+c = "5.6"
+print(float(c))
+print(int(float(c)))
+print(type(float(c)))
+
+d = 5
+print(complex(d))
+
+f = 5.6
+print(complex(f))
