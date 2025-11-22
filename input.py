fec6af6a2dc1ce72317f5b194de79479f663b959
saim-a11y <saim@thefivestarschool.org>
2025-11-22 06:56:10 +0000

    Comment out age and percentage input sections for future implementation

--- input.py
+++ input.py
@@ -1,27 +1,36 @@
-age = int(input("Enter your age: "))
+# age = int(input("Enter your age: "))
 
-if age >= 18:
-    if age <= 60:
-        print("you are eligible for pension")
-    else:
-        print("You are eligible for pension.")
-else:
-    print("you are less than 18 years old.")
+# if age >= 18:
+#     if age <= 60:
+#         print("you are eligible for pension")
+#     else:
+#         print("You are eligible for pension.")
+# else:
+#     print("you are less than 18 years old.")
 
 
 #  wirte a percentage progtram if percentage >= 90: then A+ and so o n 
 
-percentage = float(input("Enter your percentage: "))
+# percentage = float(input("Enter your percentage: "))
 
-if percentage >= 90 and percentage <= 100:
-    print("Grade: A+")
-elif percentage >= 80 and percentage < 90:
-    print("Grade: A")
-elif percentage >= 70 and percentage < 80:
-    print("Grade: B")
-elif percentage >= 60 and percentage < 70:
-    print("Grade: C")
-elif percentage >= 50 and percentage < 60:
-    print("Grade: D")
-else:
-    print("Grade: F")
+# if percentage >= 90 and percentage <= 100:
+#     print("Grade: A+")
+# elif percentage >= 80 and percentage < 90:
+#     print("Grade: A")
+# elif percentage >= 70 and percentage < 80:
+#     print("Grade: B")
+# elif percentage >= 60 and percentage < 70:
+#     print("Grade: C")
+# elif percentage >= 50 and percentage < 60:
+#     print("Grade: D")
+# else:
+#     print("Grade: F")
+
+even_count = 0
+n = 101
+for i in range(n):
+    if i%2 == 0:
+        even_count +=1
+        print("even : ", i)
+
+print(f'Total even numbers from 0 to {n} are {even_count} ')
